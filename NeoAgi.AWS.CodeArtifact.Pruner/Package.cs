f89dd0606980a2a1fb6ee1305ad5080e031abe48
Habikki <habikki@luvs.org>
2021-10-12 21:53:12 -0700

    Functional init app

---
+++ NeoAgi.AWS.CodeArtifact.Pruner/Package.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoAgi.AWS.CodeArtifact.Pruner
+{
+    public class Package
+    {
+        public string Name {  get; set; } = string.Empty;
+        public string Namespace { get; set; } = string.Empty;
+        public string Format {  get; set; } = string.Empty;
+        public List<PackageVersion> Versions {  get; set; } = new List<PackageVersion>();
+    }
+}
---
+++ NeoAgi.AWS.CodeArtifact.Pruner/PackageVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoAgi.AWS.CodeArtifact.Pruner
+{
+    public class PackageVersion
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
+        public string Revision { get; set; } = string.Empty;
+    }
+}
