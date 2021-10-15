2addbf1e5413cb3044424ea4fc230aa4cc50931a
Robert Lacy <habikki@luvs.org>
2021-10-14 21:39:51 -0700

    Scaffolding for GenericHostBuilder

--- NeoAgi.AWS.CodeArtifact.Pruner/Program.cs
+++ NeoAgi.AWS.CodeArtifact.Pruner/Program.cs
@@ -2,112 +2,132 @@
 
 using Amazon.CodeArtifact;
 using Amazon.CodeArtifact.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NeoAgi.AWS.CodeArtifact.Pruner;
 using NeoAgi.Runtime.Serialization.Json;
 
-var client = new AmazonCodeArtifactClient(new AmazonCodeArtifactConfig()
+public class Program
 {
-    RegionEndpoint = Amazon.RegionEndpoint.USWest2,
-});
-
-string domain = "neoagi";
-string repository = "nuget-store";
-string cache = $"h:\\package-cache-{domain}_{repository}.json";
-
-var request = new ListPackagesRequest()
-{
-    Domain = domain,
-    Repository = repository
-};
-
-List<Package> packages = new List<Package>();
-
-if (File.Exists(cache))
-{
-    string json = File.ReadAllText(cache);
-    packages = json.FromJson<List<Package>>();
-}
-
-// Reach out to the API if we have an empty structure
-if (packages.Count == 0)
-{
-    int iterationCount = 0;
-    int iterationMax = 50;
-    while (iterationCount == 0 || (request.NextToken != null && iterationCount < iterationMax))
+    public static void Main(string[] args)
     {
-        Task<ListPackagesResponse> response = client.ListPackagesAsync(request);
-
-        foreach (var summary in response.Result.Packages)
-        {
-            Package collection = new Package()
-            {
-                Name = summary.Package,
-                Format = summary.Format.Value
-            };
-
-            Console.WriteLine($"{summary.Package}");
-            var versions = client.ListPackageVersionsAsync(new ListPackageVersionsRequest()
-            {
-                Domain = domain,
-                Repository = repository,
-                Namespace = summary.Namespace,
-                Package = summary.Package,
-                Format = summary.Format
-            });
-
-            versions.Wait();
-            foreach (var version in versions.Result.Versions)
-            {
-                Console.WriteLine($"{version.Version} - {version.Status}");
-                collection.Versions.Add(new PackageVersion()
-                {
-                    Version = version.Version,
-                    Revision = version.Revision
-                });
-            }
-
-            packages.Add(collection);
-        }
-
-        request.NextToken = response.Result.NextToken;
-        iterationCount++;
+        CreateHostBuilder(args).Build().Run();
     }
 
-    string json = packages.ToJson();
-    File.WriteAllText(cache, json);
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<Worker>();
+            });
 }
 
-bool cacheDirty = false;
-foreach (var package in packages)
-{
-    if(package.Versions.Count > 1)
-    {
-        Console.WriteLine($"{package.Name} has {package.Versions.Count} versions.");
-        List<string> versionsToDelete = new List<string>(package.Versions.Count - 1);
 
-        foreach(var version in package.Versions.OrderBy(x => x.Version).SkipLast(1))
+/*
+ var client = new AmazonCodeArtifactClient(new AmazonCodeArtifactConfig()
         {
-            versionsToDelete.Add(version.Version);
-        }
-
-        Task delete = client.DeletePackageVersionsAsync(new DeletePackageVersionsRequest()
-        {
-            Domain = domain,
-            // Namespace = package.Namespace,
-            Package = package.Name,
-            Repository = repository,
-            Format = package.Format,
-            Versions = versionsToDelete
+            RegionEndpoint = Amazon.RegionEndpoint.USWest2,
         });
 
-        Console.WriteLine($"Scheduling the deletion of {package.Name} versions {string.Join(", ", versionsToDelete)}");
-        delete.Wait();
+        string domain = "neoagi";
+        string repository = "nuget-store";
+        string cache = $"h:\\package-cache-{domain}_{repository}.json";
 
-        cacheDirty = true;
-    }
-}
+        var request = new ListPackagesRequest()
+        {
+            Domain = domain,
+            Repository = repository
+        };
 
-if(cacheDirty)
-{
-    File.Delete(cache);
-}
+        List<Package> packages = new List<Package>();
+
+        if (File.Exists(cache))
+        {
+            string json = File.ReadAllText(cache);
+            packages = json.FromJson<List<Package>>();
+        }
+
+        // Reach out to the API if we have an empty structure
+        if (packages.Count == 0)
+        {
+            int iterationCount = 0;
+            int iterationMax = 50;
+            while (iterationCount == 0 || (request.NextToken != null && iterationCount < iterationMax))
+            {
+                Task<ListPackagesResponse> response = client.ListPackagesAsync(request);
+
+                foreach (PackageSummary summary in response.Result.Packages)
+                {
+                    Package collection = new Package()
+                    {
+                        Name = summary.Package,
+                        Format = summary.Format.Value
+                    };
+
+                    Console.WriteLine($"{summary.Package}");
+                    Task<ListPackageVersionsResponse> versions = client.ListPackageVersionsAsync(new ListPackageVersionsRequest()
+                    {
+                        Domain = domain,
+                        Repository = repository,
+                        Namespace = summary.Namespace,
+                        Package = summary.Package,
+                        Format = summary.Format
+                    });
+
+                    versions.Wait();
+                    foreach (PackageVersionSummary version in versions.Result.Versions)
+                    {
+                        Console.WriteLine($"{version.Version} - {version.Status}");
+                        collection.Versions.Add(new PackageVersion()
+                        {
+                            Version = version.Version,
+                            Revision = version.Revision
+                        });
+                    }
+
+                    packages.Add(collection);
+                }
+
+                request.NextToken = response.Result.NextToken;
+                iterationCount++;
+            }
+
+            string json = packages.ToJson();
+            File.WriteAllText(cache, json);
+        }
+
+        bool cacheDirty = false;
+        foreach (var package in packages)
+        {
+            if (package.Versions.Count > 1)
+            {
+                Console.WriteLine($"{package.Name} has {package.Versions.Count} versions.");
+                List<string> versionsToDelete = new List<string>(package.Versions.Count - 1);
+
+                foreach (var version in package.Versions.OrderBy(x => x.Version).SkipLast(1))
+                {
+                    versionsToDelete.Add(version.Version);
+                }
+
+                Task delete = client.DeletePackageVersionsAsync(new DeletePackageVersionsRequest()
+                {
+                    Domain = domain,
+                    // Namespace = package.Namespace,
+                    Package = package.Name,
+                    Repository = repository,
+                    Format = package.Format,
+                    Versions = versionsToDelete
+                });
+
+                Console.WriteLine($"Scheduling the deletion of {package.Name} versions {string.Join(", ", versionsToDelete)}");
+                delete.Wait();
+
+                cacheDirty = true;
+            }
+        }
+
+        if (cacheDirty)
+        {
+            File.Delete(cache);
+        }
+*/
---
+++ NeoAgi.AWS.CodeArtifact.Pruner/Worker.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoAgi.AWS.CodeArtifact.Pruner
+{
+    public class Worker : IHostedService
+    {
+        private readonly ILogger Logger;
+
+        public Worker(ILogger<Worker> logger, IHostApplicationLifetime appLifetime)
+        {
+            Logger = logger;
+
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopping.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("1. StartAsync has been called.");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("4. StopAsync has been called.");
+
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            Logger.LogInformation("2. OnStarted has been called.");
+        }
+
+        private void OnStopping()
+        {
+            Logger.LogInformation("3. OnStopping has been called.");
+        }
+
+        private void OnStopped()
+        {
+            Logger.LogInformation("5. OnStopped has been called.");
+        }
+    }
+}
