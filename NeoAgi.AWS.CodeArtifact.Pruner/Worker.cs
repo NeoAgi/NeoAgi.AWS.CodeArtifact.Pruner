e11c27ec73314ae9c1d7e4fdd864d2bfdd2907e7
Robert Lacy <habikki@luvs.org>
2021-10-31 06:32:40 -0700

    Package Updates

--- NeoAgi.AWS.CodeArtifact.Pruner/Worker.cs
+++ NeoAgi.AWS.CodeArtifact.Pruner/Worker.cs
@@ -3,13 +3,13 @@ using Amazon.CodeArtifact.Model;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
-using NeoAgi.Runtime.Serialization.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using NeoAgi.Text.Json;
 
 namespace NeoAgi.AWS.CodeArtifact.Pruner
 {
